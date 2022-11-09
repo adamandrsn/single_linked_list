519d9ad9dc43251a6d270649f95ba149c23239e7
Adam Anderson <adam.anderson.ft21@mail.umy.ac.id>
2022-11-09 17:31:54 +0700

    membuat method Search

--- single_linked_list/Program.cs
+++ single_linked_list/Program.cs
@@ -75,6 +75,23 @@ namespace single_linked_list
                 START = START.next;
             return true;
         }
+        /*Method untuk menge-check apakah node yang dimaksud ada di dalam list*/
+        public bool Search(int nim, ref Node previous, ref Node current)
+        {
+            previous = START;
+            current = START;
+
+            while ((current != null) && (nim != current.noMhs))
+            {
+                previous = current;
+                current = current.next;
+            }
+
+            if (current == null)
+                return (false);
+            else
+                return (true);
+        }
     }
     
  }
